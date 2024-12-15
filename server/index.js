c9d981ca32c38aa9e76947ec04077abfbd8c5386
Ekakash18 <ekakashkumar4@gmail.com>
2024-12-15 19:05:10 +0900

    done again

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const port = process.env.PORT
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({limit:'500mb'}));
 app.use(cors({
+   credentials: true,
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
